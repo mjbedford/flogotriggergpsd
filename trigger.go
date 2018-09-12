fb801e0f5a8f0e7b56e27f9deee2f41ee286b8fe
mjbedford <mbedford@tibco.com>
2018-09-12 14:23:48 +0100

    Add files via upload

--- trigger.go
+++ trigger.go
@@ -43,7 +43,8 @@ func (t *MyTrigger) Metadata() *trigger.Metadata {
 // Start implements trigger.Trigger.Start
 func (t *MyTrigger) Start() error {
 	log.Info("Trigger.Start")
-	filterName := context.GetInput("filter").(string)
+	filterName :=t.config.GetSetting("filter")
+	//filterName := context.GetInput("filter").(string)
 	log.Info("Filter : " + filterName)
 	gps, err := gpsd.Dial("localhost:2947")
 	log.Info("After Dial")
